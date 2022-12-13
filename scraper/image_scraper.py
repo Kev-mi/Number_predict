d72466158a68e4699f56dc8e8e1531fc1cb7f583
Kev-mi <90063807+Kev-mi@users.noreply.github.com>
2022-12-13 20:38:34 +0100

    Update image_scraper.py

--- scraper/image_scraper.py
+++ scraper/image_scraper.py
@@ -10,10 +10,14 @@ import os
 import urllib.request
 
 
-def img_downloader(number_of_images, driver):
+def img_downloader(number_of_images, sel_number, driver):
     for image in range(1, number_of_images+1):
         image_url = driver.find_element(By.XPATH, f'//*[@id="islrg"]/div[1]/div[{image}]/a[1]/div[1]/img').get_attribute('src')
-        urllib.request.urlretrieve(image_url, os.getcwd() + "/images/" + f"{image}.png")
+        if not os.path.exists(os.getcwd() + f"/images/{sel_number}"):
+            os.makedirs(os.getcwd() + f"/images/{sel_number}" )
+            urllib.request.urlretrieve(image_url, os.getcwd() + f"/images/{sel_number}/" + f"{image}.png")
+        else:
+            urllib.request.urlretrieve(image_url, os.getcwd() + f"/images/{sel_number}/" + f"{image}.png")
 
 
 def get_driver(website_url):
@@ -27,8 +31,8 @@ def get_driver(website_url):
 
 def number_image_scraper():
     sel_number = st.sidebar.selectbox('Select number to scrape', (0, 1, 2, 3, 4, 5, 6, 7, 8, 9))
-    number_of_images = st.sidebar.slider('select how many images to download', 0, 10, 1)
-    numbers = {0: "Number zero", 1: "Number one", 2: "Number two", 3: "Number three", 4: "Number four", 5: "Number five", 6: "Number six", 7: "Number seven", 8: "Number eight", 9: "Number nine"}
+    number_of_images = st.sidebar.slider('select how many images to download', 0, 24, 1)
+    numbers = {0: "Number zero", 1: "Number one", 2: "Number two", 3: "Number three", 4: "Number four", 5: "Number 5", 6: "Number six", 7: "Number seven", 8: "Number eight", 9: "Number nine"}
     website_url = 'https://www.google.com/imghp'
     if st.button('click here to start scraper'):
         driver = get_driver(website_url)
@@ -36,5 +40,5 @@ def number_image_scraper():
         search_box.click()
         search_box.send_keys(numbers[sel_number])
         search_box.send_keys(Keys.ENTER)
-        img_downloader(number_of_images, driver)
+        img_downloader(number_of_images, sel_number, driver)
 
