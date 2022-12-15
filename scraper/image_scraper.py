8ccadf69104fdb882c603cf872de9877c25583ba
Kev-mi <90063807+Kev-mi@users.noreply.github.com>
2022-12-15 19:48:36 +0100

    Update image_scraper.py

--- scraper/image_scraper.py
+++ scraper/image_scraper.py
@@ -8,16 +8,19 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import os
 import urllib.request
+import math
 
 
-def img_downloader(number_of_images, sel_number, driver):
-    for image in range(1, number_of_images+1):
+def img_downloader(number_of_images, sel_number, search_term, driver):
+    for image in range(1, number_of_images):
         image_url = driver.find_element(By.XPATH, f'//*[@id="islrg"]/div[1]/div[{image}]/a[1]/div[1]/img').get_attribute('src')
         if not os.path.exists(os.getcwd() + f"/images/{sel_number}"):
-            os.makedirs(os.getcwd() + f"/images/{sel_number}" )
-            urllib.request.urlretrieve(image_url, os.getcwd() + f"/images/{sel_number}/" + f"{image}.png")
+            os.makedirs(os.getcwd() + f"/images/{sel_number}")
+            urllib.request.urlretrieve(image_url, os.getcwd() + f"/images/{sel_number}/" + f"{search_term}{image}.png")
         else:
-            urllib.request.urlretrieve(image_url, os.getcwd() + f"/images/{sel_number}/" + f"{image}.png")
+            urllib.request.urlretrieve(image_url, os.getcwd() + f"/images/{sel_number}/" + f"{search_term}{image}.png")
+    driver.back()
+
 
 
 def get_driver(website_url):
@@ -31,14 +34,24 @@ def get_driver(website_url):
 
 def number_image_scraper():
     sel_number = st.sidebar.selectbox('Select number to scrape', (0, 1, 2, 3, 4, 5, 6, 7, 8, 9))
-    number_of_images = st.sidebar.slider('select how many images to download', 0, 24, 1)
-    numbers = {0: "Number zero", 1: "Number one", 2: "Number two", 3: "Number three", 4: "Number four", 5: "Number 5", 6: "Number six", 7: "Number seven", 8: "Number eight", 9: "Number nine"}
+    number_of_images = st.sidebar.slider('select how many images to download', 1, 24, 1)
+    numbers = {0: ("Number zero", "pictures of number 0", "pictures of number zero", "number 0", "siffran 0", "siffran noll", "bilder på siffran noll", "bilder på siffran 0"),
+               1: ("Number one", "pictures of number 1", "pictures of number one", "number 1", "siffran 1", "siffran ett", "bilder på siffran ett", "bilder på siffran 1"),
+               2: ("Number two", "pictures of number 2", "pictures of number two", "number 2", "siffran 2", "siffran två", "bilder på siffran två", "bilder på siffran 2"),
+               3: ("Number three", "pictures of number 3", "pictures of number three", "number 3", "siffran 3", "siffran tre", "bilder på siffran tre", "bilder på siffran 3"),
+               4: ("Number four", "pictures of number 4", "pictures of number four", "number 4", "siffran 4", "siffran fyra", "bilder på siffran fyra", "bilder på siffran 4"),
+               5: ("Number five", "pictures of number 5", "pictures of number five", "number 5", "siffran 5", "siffran fem", "bilder på siffran fem", "bilder på siffran 5"),
+               6: ("Number six", "pictures of number 6", "pictures of number six", "number 6", "siffran 6", "siffran sex", "bilder på siffran sex", "bilder på siffran 6"),
+               7: ("Number seven", "pictures of number 7", "pictures of number seven", "number 7", "siffran 7", "siffran sju", "bilder på siffran sju", "bilder på siffran 7"),
+               8: ("Number eight", "pictures of number 8", "pictures of number eight", "number 8", "siffran 8", "siffran åtta", "bilder på siffran åtta", "bilder på siffran 8"),
+               9: ("Number nine", "pictures of number 9", "pictures of number nine", "number 9", "siffran 9", "siffran nio", "bilder på siffran nio", "bilder på siffran 9")}
     website_url = 'https://www.google.com/imghp'
     if st.button('click here to start scraper'):
         driver = get_driver(website_url)
         search_box = driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')
         search_box.click()
-        search_box.send_keys(numbers[sel_number])
-        search_box.send_keys(Keys.ENTER)
-        img_downloader(number_of_images, sel_number, driver)
+        for search_term in numbers[sel_number]:
+            search_box.send_keys(search_term)
+            search_box.send_keys(Keys.ENTER)
+            img_downloader(number_of_images, sel_number, search_term, driver)
 
