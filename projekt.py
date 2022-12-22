673dd7bc598ee0aa5611c2c1e04fe0e2dc9bf023
Kev-mi <90063807+Kev-mi@users.noreply.github.com>
2022-12-22 20:59:27 +0100

    Update projekt.py

--- projekt.py
+++ projekt.py
@@ -6,7 +6,7 @@ import scraper.image_scraper as scraper
 
 def main():
     st.set_page_config(layout="wide")
-    sidebar_1_selected_option = st.sidebar.selectbox('what would you like to do', ("Draw number", "Train model", "Load model", "Upload number image", "Webscrape number image"))
+    sidebar_1_selected_option = st.sidebar.selectbox('what would you like to do', ("Draw number", "Train model", "Upload number image", "Webscrape number image"))
     sidebar_1_options = {"Draw number": webdesign.draw_number, "Train model": models.train_model, "Upload number image": webdesign.upload_number_image, "Webscrape number image": scraper.number_image_scraper}
     sidebar_1_options.get(sidebar_1_selected_option)()
 
